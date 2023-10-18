a01d77f980fb08ea9b00194b32ad6bcbe330480b
Pritam Suwal Shrestha <shresthapritam7@gmail.com>
2023-10-18 15:36:22 +0800

    Correct so far

--- scatter-demo.c
+++ scatter-demo.c
@@ -1,273 +1,5 @@
-//////#include <stdio.h>
-//////#include <stdlib.h>
-//////#include <string.h>
-//////#include <mpi.h>
-//////
-//////#define DATA_SIZE 22
-//////
-//////typedef struct {
-//////    double x, y;
-//////    double distanceTraveled;
-//////    double weight;
-//////} Fish;
-//////
-//////int main(int argc, char **argv) {
-//////    MPI_Init(&argc, &argv);
-//////
-//////    int rank, size;
-//////    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-//////    MPI_Comm_size(MPI_COMM_WORLD, &size);
-//////
-//////    Fish *data = NULL;
-//////    int data_per_process = DATA_SIZE / size;
-//////    Fish *recv_buffer = (Fish *) malloc(data_per_process * sizeof(Fish));
-//////
-//////    // Master process generates Fish data
-//////    if (rank == 0) {
-//////        data = (Fish *) malloc(DATA_SIZE * sizeof(Fish));
-//////        for (int i = 0; i < DATA_SIZE; i++) {
-//////            data[i].x = i * 0.5;
-//////            data[i].y = i * 0.5;
-//////            data[i].distanceTraveled = 0.0;
-//////            data[i].weight = 1.0;
-//////        }
-//////
-//////        // Output data to a file
-//////        FILE *file = fopen("data_before.txt", "w");
-//////        for (int i = 0; i < DATA_SIZE; i++) {
-//////            fprintf(file, "x: %lf, y: %lf, distance: %lf, weight: %lf\n",
-//////                    data[i].x, data[i].y, data[i].distanceTraveled, data[i].weight);
-//////        }
-//////        fclose(file);
-//////    }
-//////
-//////    // Distribute Fish data to all processes
-//////    if (rank == 0) {
-//////        for (int dest = 0; dest < size; dest++) {
-//////            if (dest != 0) {
-//////                MPI_Send(data + dest * data_per_process, data_per_process * sizeof(Fish), MPI_BYTE, dest, 0,
-//////                         MPI_COMM_WORLD);
-//////            }
-//////        }
-//////    } else {
-//////        MPI_Recv(recv_buffer, data_per_process * sizeof(Fish), MPI_BYTE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-//////    }
-//////
-//////    // Workers send Fish data back to the master
-//////    if (rank != 0) {
-//////        MPI_Send(recv_buffer, data_per_process * sizeof(Fish), MPI_BYTE, 0, 1, MPI_COMM_WORLD);
-//////    } else {
-//////        for (int source = 1; source < size; source++) {
-//////            MPI_Recv(data + source * data_per_process, data_per_process * sizeof(Fish), MPI_BYTE, source, 1,
-//////                     MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-//////        }
-//////
-//////        // Master process outputs the received Fish data to a file
-//////        FILE *file = fopen("data_after.txt", "w");
-//////        for (int i = 0; i < DATA_SIZE; i++) {
-//////            fprintf(file, "x: %lf, y: %lf, distance: %lf, weight: %lf\n",
-//////                    data[i].x, data[i].y, data[i].distanceTraveled, data[i].weight);
-//////        }
-//////        fclose(file);
-//////
-//////        free(data);
-//////    }
-//////
-//////    free(recv_buffer);
-//////    MPI_Finalize();
-//////
-//////    return 0;
-//////}
-////
-////
-////#include <stdio.h>
-////#include <stdlib.h>
-////#include <string.h>
-////#include <mpi.h>
-////
-////#define DATA_SIZE 22
-////
-////typedef struct {
-////    double x, y;
-////    double distanceTraveled;
-////    double weight;
-////} Fish;
-////
-////int main(int argc, char **argv) {
-////    MPI_Init(&argc, &argv);
-////
-////    int rank, size;
-////    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-////    MPI_Comm_size(MPI_COMM_WORLD, &size);
-////
-////    Fish *data = NULL;
-////    int data_per_process = DATA_SIZE / size;
-////    Fish *recv_buffer = (Fish *) malloc(data_per_process * sizeof(Fish));
-////
-////    // Master process generates Fish data
-////    if (rank == 0) {
-////        data = (Fish *) malloc(DATA_SIZE * sizeof(Fish));
-////        for (int i = 0; i < DATA_SIZE; i++) {
-////            data[i].x = i * 0.5;
-////            data[i].y = i * 0.5;
-////            data[i].distanceTraveled = 0.0;
-////            data[i].weight = 1.0;
-////        }
-////
-////        // Output data to a file
-////        FILE *file = fopen("data_before.txt", "w");
-////        for (int i = 0; i < DATA_SIZE; i++) {
-////            fprintf(file, "x: %lf, y: %lf, distance: %lf, weight: %lf\n",
-////                    data[i].x, data[i].y, data[i].distanceTraveled, data[i].weight);
-////        }
-////        fclose(file);
-////    }
-////
-////    // Distribute Fish data to all processes
-////    if (rank == 0) {
-////        for (int dest = 0; dest < size; dest++) {
-////            if (dest != 0) {
-////                // Double the x and y fields before sending
-////                for (int i = dest * data_per_process; i < (dest + 1) * data_per_process; i++) {
-////                    data[i].x *= 2.0;
-////                    data[i].y *= 2.0;
-////                    // You can similarly double other fields like distanceTraveled and weight if needed.
-////                }
-////                MPI_Send(data + dest * data_per_process, data_per_process * sizeof(Fish), MPI_BYTE, dest, 0,
-////                         MPI_COMM_WORLD);
-////            }
-////        }
-////    } else {
-////        MPI_Recv(recv_buffer, data_per_process * sizeof(Fish), MPI_BYTE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-////    }
-////
-////    // Workers send Fish data back to the master
-////    if (rank != 0) {
-////        MPI_Send(recv_buffer, data_per_process * sizeof(Fish), MPI_BYTE, 0, 1, MPI_COMM_WORLD);
-////    } else {
-////        for (int source = 1; source < size; source++) {
-////            MPI_Recv(data + source * data_per_process, data_per_process * sizeof(Fish), MPI_BYTE, source, 1,
-////                     MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-////        }
-////
-////        // Master process outputs the received Fish data to a file
-////        FILE *file = fopen("data_after.txt", "w");
-////        for (int i = 0; i < DATA_SIZE; i++) {
-////            fprintf(file, "x: %lf, y: %lf, distance: %lf, weight: %lf\n",
-////                    data[i].x, data[i].y, data[i].distanceTraveled, data[i].weight);
-////        }
-////        fclose(file);
-////
-////        free(data);
-////    }
-////
-////    free(recv_buffer);
-////    MPI_Finalize();
-////
-////    return 0;
-////}
-//
-//
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
-//#include <mpi.h>
-//
-//#define DATA_SIZE 22
-//
-//typedef struct {
-//    double x, y;
-//    double distanceTraveled;
-//    double weight;
-//} Fish;
-//
-//int main(int argc, char **argv) {
-//    MPI_Init(&argc, &argv);
-//
-//    int rank, size;
-//    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-//    MPI_Comm_size(MPI_COMM_WORLD, &size);
-//
-//    Fish *data = NULL;
-//    int data_per_process = DATA_SIZE / size;
-//    Fish *recv_buffer = (Fish *) malloc(data_per_process * sizeof(Fish));
-//
-//    // Master process generates Fish data
-//    if (rank == 0) {
-//        data = (Fish *) malloc(DATA_SIZE * sizeof(Fish));
-//        for (int i = 0; i < DATA_SIZE; i++) {
-//            data[i].x = i * 0.5;
-//            data[i].y = i * 0.5;
-//            data[i].distanceTraveled = 0.0;
-//            data[i].weight = 1.0;
-//        }
-//
-//        // Output data to a file
-//        FILE *file = fopen("data_before.txt", "w");
-//        for (int i = 0; i < DATA_SIZE; i++) {
-//            fprintf(file, "x: %lf, y: %lf, distance: %lf, weight: %lf\n",
-//                    data[i].x, data[i].y, data[i].distanceTraveled, data[i].weight);
-//        }
-//        fclose(file);
-//    }
-//
-//    // Distribute Fish data to all processes
-//    if (rank == 0) {
-//        for (int dest = 0; dest < size; dest++) {
-//            if (dest != 0) {
-//                // Double the x and y fields before sending
-//                for (int i = dest * data_per_process; i < (dest + 1) * data_per_process; i++) {
-//                    data[i].x *= 2.0;
-//                    data[i].y *= 2.0;
-//                    // You can similarly double other fields like distanceTraveled and weight if needed.
-//                }
-//                MPI_Send(data + dest * data_per_process, data_per_process * sizeof(Fish), MPI_BYTE, dest, 0,
-//                         MPI_COMM_WORLD);
-//            }
-//        }
-//    } else {
-//        MPI_Recv(recv_buffer, data_per_process * sizeof(Fish), MPI_BYTE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-//
-//        // Double the received data before sending it back
-//        for (int i = 0; i < data_per_process; i++) {
-//            recv_buffer[i].x *= 2.0;
-//            recv_buffer[i].y *= 2.0;
-//            // You can similarly double other fields like distanceTraveled and weight if needed.
-//        }
-//        MPI_Send(recv_buffer, data_per_process * sizeof(Fish), MPI_BYTE, 0, 1, MPI_COMM_WORLD);
-//    }
-//
-//    // Workers send Fish data back to the master
-//    if (rank != 0) {
-//        // No need to double the data here; it's already doubled by worker processes
-//        MPI_Send(recv_buffer, data_per_process * sizeof(Fish), MPI_BYTE, 0, 1, MPI_COMM_WORLD);
-//    } else {
-//        for (int source = 1; source < size; source++) {
-//            MPI_Recv(data + source * data_per_process, data_per_process * sizeof(Fish), MPI_BYTE, source, 1,
-//                     MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-//        }
-//
-//        // Master process outputs the received Fish data to a file
-//        FILE *file = fopen("data_after.txt", "w");
-//        for (int i = 0; i < DATA_SIZE; i++) {
-//            fprintf(file, "x: %lf, y: %lf, distance: %lf, weight: %lf\n",
-//                    data[i].x, data[i].y, data[i].distanceTraveled, data[i].weight);
-//        }
-//        fclose(file);
-//
-//        free(data);
-//    }
-//
-//    free(recv_buffer);
-//    MPI_Finalize();
-//
-//    return 0;
-//}
-
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <mpi.h>
 
 #define DATA_SIZE 22
@@ -311,33 +43,28 @@ int main(int argc, char **argv) {
     // Distribute Fish data to all processes
     if (rank == 0) {
         for (int dest = 0; dest < size; dest++) {
-            if (dest != 0) {
-                // Double the x and y fields before sending
-                for (int i = dest * data_per_process; i < (dest + 1) * data_per_process; i++) {
-                    data[i].x *= 2.0;
-                    data[i].y *= 2.0;
-                    // You can similarly double other fields like distanceTraveled and weight if needed.
-                }
-                MPI_Send(data + dest * data_per_process, data_per_process * sizeof(Fish), MPI_BYTE, dest, 0,
-                         MPI_COMM_WORLD);
-            }
+            // Send data to other processes
+            MPI_Send(data + dest * data_per_process, data_per_process * sizeof(Fish), MPI_BYTE, dest, 0,
+                     MPI_COMM_WORLD);
         }
     } else {
+        // Receive data
         MPI_Recv(recv_buffer, data_per_process * sizeof(Fish), MPI_BYTE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
 
-        // Double the received data before sending it back
+    // Add a barrier to ensure that all processes have received their data
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // Double the received data before sending it back
+    if (rank != 0) {
         for (int i = 0; i < data_per_process; i++) {
             recv_buffer[i].x *= 2.0;
             recv_buffer[i].y *= 2.0;
-            // You can similarly double other fields like distanceTraveled and weight if needed.
         }
-        MPI_Send(recv_buffer, data_per_process * sizeof(Fish), MPI_BYTE, 0, 1, MPI_COMM_WORLD);
-    }
-
-    // Workers send Fish data back to the master
-    if (rank != 0) {
+        // Send the modified data back to the master
         MPI_Send(recv_buffer, data_per_process * sizeof(Fish), MPI_BYTE, 0, 1, MPI_COMM_WORLD);
     } else {
+        // Master receives data from workers
         for (int source = 1; source < size; source++) {
             MPI_Recv(data + source * data_per_process, data_per_process * sizeof(Fish), MPI_BYTE, source, 1,
                      MPI_COMM_WORLD, MPI_STATUS_IGNORE);
--- send-recv-squared.c
+++ send-recv-squared.c
@@ -1,12 +1,68 @@
+////
+//// Created by Pritam Suwal Shrestha on 17/10/2023.
+////
 //
-// Created by Pritam Suwal Shrestha on 17/10/2023.
 //
-
+//#include <mpi.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//
+//int main(int argc, char *argv[]) {
+//    int rank, num_processes;
+//
+//    MPI_Init(&argc, &argv);
+//
+//    MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
+//    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+//
+//
+////    int i = 0;
+////    while (!i)
+////        sleep(2);
+//
+//
+//    if (rank == 0) {
+//
+//        for (int i = 0; i < num_processes; i++) {
+//            // generate random number
+////            generate random number between 1 to 10
+//            int random_number = (rand() % 10) + 1;
+//
+//            printf("Sending %d to %d\n", random_number, i);
+//
+//            MPI_Send(&random_number, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+//        }
+//
+//
+//        for (int i = 0; i < num_processes; i++) {
+//            int recv_sq;
+//            MPI_Recv(&recv_sq, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+//            printf("Received %d from %d\n", recv_sq, i);
+//        }
+//
+//    } else {
+//
+//        int recv;
+//        MPI_Recv(&recv, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+////        printf("Received %d from 0\n", recv);
+//
+//
+//        int squared = recv * recv;
+//
+//        MPI_Send(&squared, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+//    }
+//
+//
+//    MPI_Finalize();
+//}
 
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
+#define NUM_DATA_ITEMS 5
 
 int main(int argc, char *argv[]) {
     int rank, num_processes;
@@ -16,43 +72,45 @@ int main(int argc, char *argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-
-//    int i = 0;
-//    while (!i)
-//        sleep(2);
-
-
     if (rank == 0) {
+        // Create an array to store the random numbers
+        int random_numbers[NUM_DATA_ITEMS];
 
-        for (int i = 0; i < num_processes; i++) {
-            // generate random number
-//            generate random number between 1 to 10
+        // Seed the random number generator
+        srand(time(NULL));
+
+        // Generate and send random numbers to other processes
+        for (int i = 0; i < NUM_DATA_ITEMS; i++) {
             int random_number = (rand() % 10) + 1;
+            random_numbers[i] = random_number;
 
-            printf("Sending %d to %d\n", random_number, i);
-
-            MPI_Send(&random_number, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            for (int dest = 1; dest < num_processes; dest++) {
+                MPI_Send(&random_number, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
+            }
         }
 
-
-        for (int i = 0; i < num_processes; i++) {
-            int recv_sq;
-            MPI_Recv(&recv_sq, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            printf("Received %d from %d\n", recv_sq, i);
+        // Receive and print squared values from other processes
+        for (int i = 0; i < NUM_DATA_ITEMS; i++) {
+            int squared_values[num_processes - 1];
+            for (int src = 1; src < num_processes; src++) {
+                int recv_sq;
+                MPI_Recv(&recv_sq, 1, MPI_INT, src, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                squared_values[src - 1] = recv_sq;
+            }
+            printf("Random Number %d: %d\n", i, random_numbers[i]);
+            for (int src = 1; src < num_processes; src++) {
+                printf("Received squared value from Process %d: %d\n", src, squared_values[src - 1]);
+            }
         }
-
     } else {
-
+        // Receive random numbers and send squared values to the master process
         int recv;
-        MPI_Recv(&recv, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-//        printf("Received %d from 0\n", recv);
-
-
-        int squared = recv * recv;
-
-        MPI_Send(&squared, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        for (int i = 0; i < NUM_DATA_ITEMS; i++) {
+            MPI_Recv(&recv, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            int squared = recv * recv;
+            MPI_Send(&squared, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        }
     }
-
 
     MPI_Finalize();
 }
